55f2641e16bf4fc074c67e3ee7dac437fc0f2f38
Ronald St. Hilaire <rfs@Ronalds-MacBook-Pro.local>
2020-05-21 13:07:04 -0700

    Fix the bot Co-authored-by: Matthew_Cash <matthew@matthew-cash.com>

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const os = require('os');
 const Discord = require('discord.js');
 const googleTTS = require('google-tts-api');
 const client = new Discord.Client();
@@ -26,7 +27,13 @@ fs.readdir('./commands/', (err, data) => {
 });
 
 client.once('ready', () => {
-	console.log('Ready!');
+    console.log('Ready!');
+    const logChannel = (await bot.channels.resolve(
+        '712033733829591051'
+    ));
+    logChannel.send(
+        `**Paddy's Bot Started** NodeJS ${process.version} running on ${os.type()} ${os.release()}\n__System Info__ ${os.cpus().length} Logical Cores - ${os.totalmem()}B RAM`
+    );
 });
 
 client.on('message' , msg => {
@@ -43,8 +50,7 @@ client.on('message' , msg => {
 
     client.on('voiceStateUpdate', async (oldState, voiceState) => {
         if (oldState.channel === voiceState.channel) return; // Didn't Leave/Join
-        if (voiceState.channel) return; // Didn't Leave
-        
+        if (oldState.member.id ===  '707005983955877969') return; // REV0        
         if (client.user === oldState.member.user) return; // Is this bot
     
         const connection = await oldState.channel.join();
